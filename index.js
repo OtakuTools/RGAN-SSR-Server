f360a3e8b7bcb8b96e059e24ab5e0f1dae371e9c
ZTao-z <1124693098@qq.com>
2019-11-16 23:22:24 +0800

    add markdown

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 exports.main_handler = async (event, context, callback) => {
-    callback(null, "欢迎使用RGAN-SSR-Server云函数示例")
+    const md = require('markdown-it')()
+    let r = md.render(JSON.parse(event.body).text)
+    callback(null, r)
     // return event
 };
